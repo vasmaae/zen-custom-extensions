34898b3d4887c5f66c1e3c2c342efd64b057075e
Ivan Gutorov <gutorov.ia1@gmail.com>
2026-02-08 22:03:14 +0400

    fix: fix build script

--- chatgpt-russian-requests-block/build.sh
+++ chatgpt-russian-requests-block/build.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-zip chatgpt-russian-requests-block.xpi background.js icon.webp manifest.json
+zip chatgpt-russian-requests-block.xpi background.js options.html options.js icon.webp manifest.json
